8dd0b0958fc84c4d97953eb6bf8077a0f6300726
linyushan <1107012776@qq.com>
2020-12-06 00:55:13 +0800

    消费者需要协程go

--- tests/ConsumerTest.php
+++ tests/ConsumerTest.php
@@ -25,11 +25,12 @@ class ConsumerTest extends TestCase
      */
     public function testListen()
     {
-
-        $driver = new \EasySwoole\RabbitMq\RabbitMqQueueDriver('127.0.0.1', 5672, 'test', 'test', "/");
-        MqQueue::getInstance($driver);
-        MqQueue::getInstance()->consumer()->setConfig('kd_sms_send_ex', 'hello')->listen(function (MqJob $job) {
-            var_dump($job->getJobData());
+        go(function () {
+            $driver = new \EasySwoole\RabbitMq\RabbitMqQueueDriver('127.0.0.1', 5672, 'test', 'test', "/");
+            MqQueue::getInstance($driver);
+            MqQueue::getInstance()->consumer()->setConfig('kd_sms_send_ex', 'hello')->listen(function (MqJob $job) {
+                var_dump($job->getJobData());
+            });
         });
     }
 
